d30d167cf54a83a78c00c14c43d1e95e4a94f9f7
waverDeep <dandyleader@naver.com>
2021-08-18 17:47:22 +0900

    config 파일 생성 및 수정 위치 변경 6

--- pyclient/pol_sniffer.py
+++ pyclient/pol_sniffer.py
@@ -334,7 +334,7 @@ ui = ConsoleUI()
 filter_mac = bytearray(6)
 
 # get path to config file
-script_path = "/home/pi/Documents/raccoon/pyclient"
+script_path = "/home/pi/Documents/raccoon-master/pyclient"
 config_path = config_name
 create_config_template(script_path+'/'+config_path)
 
